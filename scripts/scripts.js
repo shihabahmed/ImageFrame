d8e258228da590b1aba7d242f5c888cba6827557
Shihab Ahmed <2231724+shihabahmed@users.noreply.github.com>
2019-09-12 14:56:51 +0600

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,30 +1,30 @@
 var jWindow, imageFrame1, imageFrame2;
 
 var Elements = (function (j) {
-	return {
-		Page: j('.landing-page'),
-		PageLoader: j('.page-loader')
-	}
+    return {
+        Page: j('.landing-page'),
+        PageLoader: j('.page-loader')
+    }
 })(jQuery);
 
-; (function (j) {
-	j(function () {
-		jWindow = j(window);
+;(function (j) {
+    j(function () {
+        jWindow = j(window);
 
-		jWindow.load(function () {
-			Elements.Page.animate({
-				'opacity': 1
-			}, 600);
+        jWindow.load(function () {
+            Elements.Page.animate({
+                'opacity': 1
+            }, 600);
 
-			Elements.PageLoader.fadeOut(function () {
-				imageFrame1 = j('.image-frame1').imageframe();
-				imageFrame2 = j('.image-frame2').imageframe({
-					interval: 600,
-					duration: 300
-				});
-			});
-		}).resize(function () {
-			Elements.PageLoader.height(jWindow.height());
-		}).resize();
-	});
+            Elements.PageLoader.fadeOut(function () {
+                imageFrame1 = j('.image-frame1').imageframe();
+                imageFrame2 = j('.image-frame2').imageframe({
+                    interval: 600,
+                    duration: 300
+                });
+            });
+        }).resize(function () {
+            Elements.PageLoader.height(jWindow.height());
+        }).resize();
+    });
 })(jQuery);
